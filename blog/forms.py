dd4c38660cef54fe0ac52411f40d1160e30a29eb
Jordan Koch <jordan.marie.koch@gmail.com>
2018-11-01 15:10:52 -0500

    added a form for smallIntPositive integer

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,4 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'text', 'n_floatNum', )
+        fields = ('title', 'text', 'n_floatNum', 'n_smallIntNum', )
