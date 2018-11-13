224c3bf6086aa3beae72f28e1663760eb333c051
Jordan Koch <jordan.marie.koch@gmail.com>
2018-11-13 09:16:54 -0600

    added TraitForm class to forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 #from django import forms
-from .models import Post
+from .models import Post, Trait
 from django.forms import ModelForm
 
 class PostForm(ModelForm):
@@ -7,6 +7,31 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text', 'n_floatNum', 'n_smallIntNum', 'n_boolVal', 'isi_val', )
+
+class TraitForm(ModelForm):
+    class Meta:
+        model = Trait
+        fields = ['genus', 'species', 'fruit_type']
+
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Fieldset(
+                'Taxonomy',
+                'genus',
+                'species',
+            ),
+            Fieldset(
+                'Fruit Traits',
+            InlineCheckboxes('fruit_type'),
+            ),
+            ButtonHolder(
+                Submit('create', 'Create Trait', css_class='button white')
+            )
+        )
+
+        super(TraitForm, self).__init__(*args, **kwargs)
+
 
 '''
 class MinValueValidator(BaseValidator):
