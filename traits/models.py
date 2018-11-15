ff89f86dbf4250a6b170d60ce60dae4661ad8e33
Jordan Koch <jordan.marie.koch@gmail.com>
2018-11-15 09:38:34 -0600

    added ISI minvalue and maxvalue validation

--- traits/models.py
+++ traits/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
 from crispy_forms.bootstrap import InlineCheckboxes
@@ -10,7 +11,7 @@ class Trait(models.Model):
     #    )
     genus = models.CharField(max_length=50, null=True, blank=False, )#help_text= 'Enter data if known. Expects str as input')
     species = models.CharField(max_length=50, null=True, blank=False, )
-    isi = models.FloatField(blank=True, null=True, )
+    isi = models.FloatField(blank=True, null=True, validators=[MinValueValidator(0.0, message='Must be a number between 0.0 and 1.0'), MaxValueValidator(1.0, message=None)])
     
     FRUIT_TYPE_CHOICES = (('Capsule','Capsule'),('Berry','Berry'))
     fruit_type = models.CharField(max_length=50, null=False, default='none', choices=FRUIT_TYPE_CHOICES)
