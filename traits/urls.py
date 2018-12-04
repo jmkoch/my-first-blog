7d823e6fbb76169e8304563d5521437572baf214
Jordan Koch <jordan.marie.koch@gmail.com>
2018-12-04 12:32:50 -0600

    added commented code for form rendering

--- traits/urls.py
+++ traits/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from traits.views import TraitCreateView
+from traits.views import TraitCreateView#, AddressCreateView
+#from traits.forms import AddressForm
 
 urlpatterns = [
 	url(r'^trait/create/$', TraitCreateView.as_view(), name="create trait"),
+	#url(r'^address/create/$', AddressCreateView.as_view(), name="address form"),
 ]
--- traits/views.py
+++ traits/views.py
@@ -1,6 +1,10 @@
 from django.views.generic import CreateView
 from traits.models import Trait
+#from traits.forms import AddressForm
 
 class TraitCreateView(CreateView):
 	model = Trait
 	fields = ('genus', 'species', 'isi', 'fruit_type')
+
+#class AddressCreateView(CreateView):
+#	fields = ('email', 'password', 'address_1', 'address_2', 'city', 'state', 'zip_code')
